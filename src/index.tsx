aec7dcf760cc863974b8c73112e2f61fe39aa29d
mcx <machaoxue@yeah.net>
2022-12-13 13:43:50 +0800

    feat(examples): how-to-debug

--- src/index.tsx
+++ src/index.tsx
@@ -2,10 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import router from './router';
+import { StrictMode } from 'react';
 import {
   RouterProvider,
 } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <RouterProvider router={router} />,
+  <StrictMode>
+    <RouterProvider router={router} />
+  </StrictMode>,
 );
--- src/pages/exmaples/routes.tsx
+++ src/pages/exmaples/routes.tsx
@@ -3,6 +3,7 @@ import Textures from './textures';
 import FirstScene from './first-scene';
 import PlayWithDrei from './play-with-drei';
 import { setDocumentTitle } from '../../utils';
+import HowToDebug from './how-to-debug';
 
 // 这里想写一种 TS 类型，能够扩展 RouteObject，新增 title: string，目前还没有想到一个好办法
 const routes: RouteObject[] = [
@@ -19,6 +20,14 @@ const routes: RouteObject[] = [
     element: <PlayWithDrei />,
     loader: async () => {
       setDocumentTitle('play-with-drei');
+      return null;
+    },
+  },
+  {
+    path: 'how-to-debug',
+    element: <HowToDebug />,
+    loader: async () => {
+      setDocumentTitle('how-to-debug');
       return null;
     },
   },
