a0d8c70f08afcc84220b6dfe0da33772e9a335b3
mcx <machaoxue@yeah.net>
2022-12-13 19:34:18 +0800

    feat(examples): environment-and-staging

--- src/pages/exmaples/routes.tsx
+++ src/pages/exmaples/routes.tsx
@@ -4,6 +4,7 @@ import FirstScene from './first-scene';
 import PlayWithDrei from './play-with-drei';
 import { setDocumentTitle } from '../../utils';
 import HowToDebug from './how-to-debug';
+import EnvironmentAndStaging from './environment-and-staging';
 import SphereCubePlane from './sphere-cube-plane';
 
 // 这里想写一种 TS 类型，能够扩展 RouteObject，新增 title: string，目前还没有想到一个好办法
@@ -37,6 +38,14 @@ const routes: RouteObject[] = [
     element: <HowToDebug />,
     loader: async () => {
       setDocumentTitle('how-to-debug');
+      return null;
+    },
+  },
+  {
+    path: 'environment-and-staging',
+    element: <EnvironmentAndStaging />,
+    loader: async () => {
+      setDocumentTitle('environment-and-staging');
       return null;
     },
   },
