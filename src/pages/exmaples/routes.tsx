77f7598197fa64ac317a6257c6d34722117460ac
mcx <machaoxue@yeah.net>
2022-12-15 14:36:35 +0800

    feat(examples): cube-demo

--- src/pages/exmaples/routes.tsx
+++ src/pages/exmaples/routes.tsx
@@ -8,9 +8,18 @@ import EnvironmentAndStaging from './environment-and-staging';
 import SphereCubePlane from './sphere-cube-plane';
 import LoadModels from './load-models';
 import Load3DText from './load-3d-text';
+import CubeDemo from './cube-demo';
 
 // 这里想写一种 TS 类型，能够扩展 RouteObject，新增 title: string，目前还没有想到一个好办法
 const routes: RouteObject[] = [
+  {
+    path: 'cube-demo',
+    element: <CubeDemo />,
+    loader: async () => {
+      setDocumentTitle('cube-demo');
+      return null;
+    },
+  },
   {
     path: 'sphere-cube-plane',
     element: <SphereCubePlane />,
