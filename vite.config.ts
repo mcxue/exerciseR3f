b58171f1da24405e5ec945d51adb9db67b645906
machaoxue <machaoxue@yeah.net>
2023-03-27 00:50:03 +0800

    fix(project): adjust vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,17 +3,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      external: ['react', 'react-dom', '@react-three/fiber'],
-      output: {
-        globals: {
-          react: 'react',
-          'react-dom': 'react-dom',
-          '@react-three/fiber': '@react-three/fiber',
-        },
-      },
-    },
-  },
   assetsInclude: ['**/*.glb', '**/*.hdr'],
 });
